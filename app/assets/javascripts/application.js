0a3854623dd43b81a3454f8e2b2dc62070aba7ba
Pedro Araujo <ppteixeira@outlook.com>
2017-01-16 23:50:50 +1100

    Added photos on index, menu links, and styling on chat

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -93,21 +93,21 @@ $(document).ready(function () {
 
   // Button to generate new key (pin) to a chat room.
   $( '.btn-key' ).click(function(event) {
-      event.preventDefault();  // Prevent the button to send the form
-      debugger
+    event.preventDefault();  // Prevent the button to send the form
 
-      var id = $(this).closest('li').attr('id');
-      var options = { // Details to connect to the server
-        url: '/change_pin',
-        method: 'post',
-        data: { table_id: $(this).closest('li').attr('id') }
-      }
+    var id = $(this).closest('li').attr('id');
+    var options = { // Details to connect to the server
+      url: '/change_pin',
+      method: 'post',
+      data: { table_id: $(this).closest('li').attr('id') }
+    }
 
-      var show_pin = function(response) { // function that
-        //look up for right id and write response
-        $('#'+id).find('.pin').text(response)
-      }
-      $.ajax(options).done(show_pin);
+    var show_pin = function(response) { // function that
+      //look up for right id and write response
+      $('#'+id).find('.pin').text(response)
+    }
+    $.ajax(options).done(show_pin);
+  });
 
-    });
+  
 })
--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,7 +1,6 @@
 class Api::MessagesController < ApplicationController
 
   def check_messages
-      chat_rooms = ChatRoom.all
       quantity = Messages.where(chat_room_id: params[:chat_room_id]).count
       render json: quantity.to_json
   end
