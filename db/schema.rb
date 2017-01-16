d090e483a85a0854243290551dcf426ec334e976
Pedro Araujo <ppteixeira@outlook.com>
2017-01-17 00:50:26 +1100

    run migration for items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170113084825) do
+ActiveRecord::Schema.define(version: 20170116125325) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 20170113084825) do
     t.datetime "updated_at", null: false
     t.string   "pin"
     t.index ["user_id"], name: "index_chat_rooms_on_user_id", using: :btree
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.string   "category"
+    t.decimal  "price",       precision: 8, scale: 2
+    t.string   "item_type"
+    t.text     "description"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
 
   create_table "messages", force: :cascade do |t|
